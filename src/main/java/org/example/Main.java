d686dae69d6b7a288d4d5c2df25ad91eabfff4bf
mattianicastro <nicastromattia@outlook.com>
2022-09-27 13:21:15 +0200

    log status code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,13 @@
 package org.example;
-
-
 import org.tinylog.Logger;
-
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        before("/*", (req, res) -> Logger.info("[{}] {} - {}", req.ip(), req.requestMethod(), req.pathInfo()));
+        after("/*", (req, res) -> Logger.info("[{}] {} {} - {}", req.ip(), req.requestMethod(), req.pathInfo(), res.status()));
         get("/hello", (req, res) -> "Hello World");
-        post("/test", (req, res) -> {
-            Logger.trace(req);
+        get("/test", (req, res) -> {
+            res.status(400);
             return req.ip();
         });
     }
